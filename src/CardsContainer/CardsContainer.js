fc06cef4f9932a348998fc7efd19f8ea9e965ed0
Fernando Cutire ubuntu <fernandocutire@gmail.com>
2021-07-22 21:12:29 -0500

    NEW: Contenedores - reemplazando textos en ingl╬ô├╢┬ú╬ô├«├ës por los n╬ô├╢┬ú╬ô├▓├ªmeros de ayudinga

--- src/CardsContainer/CardsContainer.js
+++ src/CardsContainer/CardsContainer.js
@@ -2,35 +2,35 @@ function CardsContainer () {
     return(
         <section className="main-cards-container">
             <div className="main-card">
-                <p className="username"><i className="fab fa-facebook-square"></i>@nathanf</p>
-                <h1 className="followers__number">1987</h1>
-                <p className="followers__title">Followers</p>
-                <p className="followers__today up"><i className="fas fa-caret-up"></i>12 Today</p>
+                <p className="username"><i className="fab fa-facebook-square"></i>@ayudinga</p>
+                <h1 className="followers__number">9.418</h1>
+                <p className="followers__title">Seguidores</p>
+                <p className="followers__today up"><i className="fas fa-caret-up"></i>12 Hoy</p>
             </div>
 
             <div className="main-card">
-                <p className="username twitter"><i className="fab fa-twitter"></i>@nathanf</p>
-                <h1 className="followers__number">1044</h1>
-                <p className="followers__title">Followers</p>
-                <p className="followers__today up"><i className="fas fa-caret-up"></i>99 Today</p>
+                <p className="username twitter"><i className="fab fa-twitter"></i>@ayudinga</p>
+                <h1 className="followers__number">7110</h1>
+                <p className="followers__title">Seguidores</p>
+                <p className="followers__today up"><i className="fas fa-caret-up"></i>15 Hoy</p>
             </div>
 
             <div className="main-card">
                 <p className="username instagram">
-                    <i className="fab fa-instagram"></i>@realnathanf
+                    <i className="fab fa-instagram"></i>@ayudinga
                 </p>
-                <h1 className="followers__number">11k</h1>
-                <p className="followers__title">Followers</p>
-                <p className="followers__today up"><i className="fas fa-caret-up"></i>1099 Today</p>
+                <h1 className="followers__number">33.8k</h1>
+                <p className="followers__title">Seguidores</p>
+                <p className="followers__today up"><i className="fas fa-caret-up"></i>20 Hoy</p>
             </div>
 
             <div className="main-card">
                 <p className="username youtube-square">
-                    <i className="fab fa-youtube-square"></i>Nathan F.
+                    <i className="fab fa-youtube-square"></i>Ayudinga!
                 </p>
-                <h1 className="followers__number">8239</h1>
-                <p className="followers__title">Subscribers</p>
-                <p className="followers__today down"><i className="fas fa-caret-down"></i>144 Today</p>
+                <h1 className="followers__number">112k</h1>
+                <p className="followers__title">Suscriptores</p>
+                <p className="followers__today down"><i className="fas fa-caret-down"></i>30 Hoy</p>
             </div>
         </section>
     );
